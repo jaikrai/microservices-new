1d22e54a0286f90cc2c3707b3b75649b19253e41
Jai <jaikrai0@gmail.com>
2022-06-22 08:00:15 -0400

    Adding api gateway

--- api-gateway/src/main/java/com/programmi9n4fun/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/programmi9n4fun/apigateway/ApiGatewayApplication.java
@@ -1,2 +1,14 @@
-package com.programmi9n4fun.apigateway;public class ApiGatewayApplication {
+package com.programmi9n4fun.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 }
