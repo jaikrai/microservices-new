a21d03e20d1b4540ac93f9d62198ade058fdcf8a
Jai <jaikrai0@gmail.com>
2022-06-24 17:16:47 -0400

    adding ditrubiut trace

--- order-service/src/main/java/com/program4fun/orderService/controller/OrderController.java
+++ order-service/src/main/java/com/program4fun/orderService/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.program4fun.orderService.controller;
 import com.program4fun.orderService.dto.OrderRequest;
 import com.program4fun.orderService.service.OrderService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -21,9 +22,9 @@ public class OrderController {
     @ResponseStatus(HttpStatus.CREATED)
     @TimeLimiter(name = "inventory")
     @CircuitBreaker(name = "inventory", fallbackMethod = "fallBackMethod")
+    @Retry(name = "inventory")
     public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest){
        return CompletableFuture.supplyAsync(()-> orderService.placeOrder(orderRequest));
-//        return CompletableFuture.supplyAsync(()->"Order placed successfully");
     }
 
     public CompletableFuture<String> fallBackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
